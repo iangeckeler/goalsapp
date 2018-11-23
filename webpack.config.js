c41c64dd5dfe2627c09e82d142822962e6b3eaec
Ian Geckeler <ikcgeckeler@gmail.com>
2018-11-22 21:32:40 -0800

    rolled back webpack to dev and added html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,34 @@
 let path = require('path');
-
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
-    entry: './src/index.js',
+    entry: './views/index.js',
     output: {
         path: path.resolve(__dirname,'dist'),
         filename: 'bundle.js'
     },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
         }
-      ]
-    }
-  };
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./views/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
