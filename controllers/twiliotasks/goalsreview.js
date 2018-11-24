63332d68117dcbcde2750298581db2a9a99306cc
Ian Geckeler <ikcgeckeler@gmail.com>
2018-11-23 23:42:48 -0800

    added phone rotation to goals review

--- controllers/twiliotasks/goalsreview.js
+++ controllers/twiliotasks/goalsreview.js
@@ -2,12 +2,16 @@ const sendTwilio = require('../../scripts/sendtwilio').sendTwilio;
 const twilioPhone = require('../../scripts/sendtwilio').twilioPhone;
 const connectDb = require('../../database').db;
 const appUrl = require('../../routes/constants').appUrl;
+const phones = require('../../routes/constants').phones;
 
-const goalsReview = (to)=>{
+
+const goalsReview = (to,index)=>{
+    //phonepool number
+    let phoneIndex = index%phones.length; 
     // argument recieved as a user
     let userPhone = '+1'+to;
     const message = "Rise and shine! Don't Forget to check out your goals this morning: "+appUrl
-    sendTwilio(twilioPhone,userPhone,message)
+    sendTwilio(phones[phoneIndex],userPhone,message)
 }
 
 
--- cron/morningreminder.js
+++ cron/morningreminder.js
@@ -5,7 +5,7 @@ findUsers().then(arr=>{
     for (let i=0;i<arr.length;i++){
     //send all users a morning reminder asking them for their key priorities
     let phone = arr[i].phone;
-    goalsReview(phone)
+    goalsReview(phone,i)
     }
 })
 
