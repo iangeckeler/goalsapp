11e0182fefcd8467720b42bc34b81747899ec736
Ian Geckeler <ikcgeckeler@gmail.com>
2018-11-25 19:36:24 -0800

    added focus app prompt in the morning

--- controllers/twiliotasks/goalsreview.js
+++ controllers/twiliotasks/goalsreview.js
@@ -3,7 +3,7 @@ const twilioPhone = require('../../scripts/sendtwilio').twilioPhone;
 const connectDb = require('../../database').db;
 const appUrl = require('../../routes/constants').appUrl;
 const phones = require('../../routes/constants').phones;
-
+const findUser = require('../../scripts/finduser')
 
 const goalsReview = (to,index)=>{
     return new Promise((resolve,reject)=>{
@@ -11,13 +11,21 @@ const goalsReview = (to,index)=>{
     let phoneIndex = index%phones.length; 
     // argument recieved as a user
     let userPhone = '+1'+to;
-    const message = "Rise and shine! Don't Forget to check out your goals this morning!"
-    sendTwilio(phones[phoneIndex],userPhone,message).then(res=>{
-        resolve(res)
+    
+    //get user for their name
+    findUser(to).then(arr=>{
+        let name = arr[0].name;
+        const message = "Rise and shine "+name+"! Get shit done today with thefocusapp! "+appUrl
+        sendTwilio(phones[phoneIndex],userPhone,message).then(res=>{
+            resolve(res)
+        }).catch(err=>{
+            reject(err)
+        })
+        })
     }).catch(err=>{
         reject(err)
     })
-    })
+
 
 }
 
