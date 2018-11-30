28a1f0916d33ea68c1864d3afb3397541f4a8525
Ian Geckeler <ikcgeckeler@gmail.com>
2018-11-29 21:43:03 -0800

    updated taskupdate to send with phoneIndex

--- controllers/twiliotasks/taskupdate.js
+++ controllers/twiliotasks/taskupdate.js
@@ -3,6 +3,7 @@ const twilioPhone = require('../../scripts/sendtwilio').twilioPhone;
 const myPhone = require('../../scripts/sendtwilio').myPhone;
 const updateMsg = require('../../scripts/updatemessage')
 const findGoal = require('../../scripts/findgoal')
+const findUser = require('../../scripts/finduser')
 const phones = require('../../routes/constants').phones;
 //set up the timeline (done in heroku) eventually switch to node-cron
 
@@ -21,15 +22,28 @@ const taskUpdate = (from,index)=>{
             console.log(arr)
             let tasks = arr[0].tasks;
             let status = arr[0].status;
-            message = updateMsg(tasks,status)
-            //send the task list
-            number = '+1'+from
-            console.log(number)
-            sendTwilio(phones[phoneIndex],number,message).then(res=>{
-                resolve('sendtwilio worked')
-            }).catch(err=>{
-                reject('sendtwiliofailed')
-            })
+            
+
+            //findUser
+            findUser(from).then(person=>{
+                let phoneIndex = person[0].phoneIndex;
+                let name = person[0].name;
+                let message = 'Hey '+name+'!\n'  
+                message += updateMsg(tasks,status)
+
+                            //send the task list
+                number = '+1'+from
+                console.log(number)
+                sendTwilio(phones[phoneIndex],number,message).then(res=>{
+                    resolve('sendtwilio worked')
+                }).catch(err=>{
+                    reject('sendtwiliofailed')
+                })
+
+
+
+            }).catch(err=>{console.log(err)})
+
         }).catch(err=>{
             reject(err)
         })
