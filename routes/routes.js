08d490487b7189d0559f9c5e917cc8eef5a32648
Ian Geckeler <ikcgeckeler@gmail.com>
2018-11-30 13:51:30 -0800

    fixed the typo in routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const sendData = require('../controllers/sendData')
 const getData = require('../controllers/getData');
-const submitted = require('../controlers/submitted')
+const submitted = require('../controllers/submitted')
 const appUrl = require('./constants').appUrl
 router.get('/',(req,res)=>{
     res.render('index.ejs',{appUrl:appUrl})
