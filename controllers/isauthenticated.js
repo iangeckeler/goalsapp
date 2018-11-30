2be54e2a6c26700c69bad7fa4f76c19bd548e3f6
Ian Geckeler <ikcgeckeler@gmail.com>
2018-11-30 13:06:18 -0800

    added changes to isauthenticated

--- controllers/isauthenticated.js
+++ controllers/isauthenticated.js
@@ -1,7 +1,7 @@
 const isAuthenticated = (req,res,next) => {
     //console.log(req.originalUrl)
     //console.log(req.session)
-    if (req.originalUrl == '/login' || req.originalUrl=='/login/' || req.originalUrl == '/login/signup'||req.originalUrl == '/sms') {
+    if (req.originalUrl.includes('login') || req.originalUrl=='/login/' || req.originalUrl == '/login/signup'||req.originalUrl == '/sms') {
         return next()
     } else if (req.session.loggedIn) {
         return next()
